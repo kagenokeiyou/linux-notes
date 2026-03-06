0ebe785d29a9db71939b24e80895f9e93b66a542
kagenokeiyou <kagenokeiyou@outlook.com>
2026-03-06 23:39:26 +0800

    fix

--- scripts/install-pkgs-arch.bash
+++ scripts/install-pkgs-arch.bash
@@ -21,20 +21,20 @@ BLUE="\033[0;34m"
 NC="\033[0m"
 
 if [[ $EUID -ne 0 ]]; then
-	echo "${RED}Please run this script with sudo or as root!${NC}"
+	echo -e "${RED}Please run this script with sudo or as root!${NC}"
 	exit 1
 fi
 
-echo "${BLUE}Updating package database...${NC}"
+echo -e "${BLUE}Updating package database...${NC}"
 pacman -Sy
 
 for pkg in "${packages[@]}"; do
 	if pacman -Qi "${pkg}" &>/dev/null; then
-		echo "${GREEN}${pkg} is already installed.${NC}"
+		echo -e "${GREEN}${pkg} is already installed.${NC}"
 	else
 		echo "${BLUE}Installing ${pkg}...${NC}"
 		pacman -S --noconfirm "${pkg}"
 	fi
 done
 
-echo "${GREEN}All tasks completed!${NC}"
+echo -e "${GREEN}All tasks completed!${NC}"
